04d45a455f71de52c6b446438445de1a16db0282
benyamin abedian <benyamin.abedian@gmail.com>
2020-08-05 15:46:42 +0200

    Change the default user to currentUser

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import './App.css';
 
 function App() {
     const [messageList, setMessages] = useState([])
-    const [user] = useState('NINJA')
+    const [user] = useState('currentUser')
     const isMedium = useWindowDimensions()
 
     useEffect(initialiseMessages, []);
